e490666f526cca475f08859383f63a37dff5ee30
solarhen <94708067+solarhen@users.noreply.github.com>
2022-01-18 09:02:14 +0200

    Add files via upload

---
+++ me (4)/util/BlockInteractionHelper.java
@@ -0,0 +1,123 @@
+// 
+// Decompiled by Procyon v0.5.36
+// 
+
+package me.zeroeightsix.kami.util;
+
+import java.util.Arrays;
+import net.minecraft.init.Blocks;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.multiplayer.PlayerControllerMP;
+import net.minecraft.block.Block;
+import net.minecraft.network.Packet;
+import net.minecraft.network.play.client.CPacketPlayer;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.Vec3i;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
+import net.minecraft.client.Minecraft;
+import java.util.List;
+
+public class BlockInteractionHelper
+{
+    public static final List blackList;
+    private static final Minecraft mc;
+    
+    public static boolean hotbarSlotCheckEmpty(final ItemStack stack) {
+        return stack != ItemStack.field_190927_a;
+    }
+    
+    public static boolean blockCheckNonBlock(final ItemStack stack) {
+        return stack.func_77973_b() instanceof ItemBlock;
+    }
+    
+    public static void placeBlockScaffold(final BlockPos pos) {
+        final Vec3d eyesPos = new Vec3d(Wrapper.getPlayer().field_70165_t, Wrapper.getPlayer().field_70163_u + Wrapper.getPlayer().func_70047_e(), Wrapper.getPlayer().field_70161_v);
+        for (final EnumFacing side : EnumFacing.values()) {
+            final BlockPos neighbor = pos.func_177972_a(side);
+            final EnumFacing side2 = side.func_176734_d();
+            if (canBeClicked(neighbor)) {
+                final Vec3d hitVec = new Vec3d((Vec3i)neighbor).func_72441_c(0.5, 0.5, 0.5).func_178787_e(new Vec3d(side2.func_176730_m()).func_186678_a(0.5));
+                if (eyesPos.func_72436_e(hitVec) <= 18.0625) {
+                    faceVectorPacketInstant(hitVec);
+                    processRightClickBlock(neighbor, side2, hitVec);
+                    Wrapper.getPlayer().func_184609_a(EnumHand.MAIN_HAND);
+                    BlockInteractionHelper.mc.field_71467_ac = 4;
+                    return;
+                }
+            }
+        }
+    }
+    
+    private static float[] getLegitRotations(final Vec3d vec) {
+        final Vec3d eyesPos = getEyesPos();
+        final double diffX = vec.field_72450_a - eyesPos.field_72450_a;
+        final double diffY = vec.field_72448_b - eyesPos.field_72448_b;
+        final double diffZ = vec.field_72449_c - eyesPos.field_72449_c;
+        final double diffXZ = Math.sqrt(diffX * diffX + diffZ * diffZ);
+        final float yaw = (float)Math.toDegrees(Math.atan2(diffZ, diffX)) - 90.0f;
+        final float pitch = (float)(-Math.toDegrees(Math.atan2(diffY, diffXZ)));
+        return new float[] { Wrapper.getPlayer().field_70177_z + MathHelper.func_76142_g(yaw - Wrapper.getPlayer().field_70177_z), Wrapper.getPlayer().field_70125_A + MathHelper.func_76142_g(pitch - Wrapper.getPlayer().field_70125_A) };
+    }
+    
+    private static Vec3d getEyesPos() {
+        return new Vec3d(Wrapper.getPlayer().field_70165_t, Wrapper.getPlayer().field_70163_u + Wrapper.getPlayer().func_70047_e(), Wrapper.getPlayer().field_70161_v);
+    }
+    
+    private static void faceVectorPacketInstant(final Vec3d vec) {
+        final float[] rotations = getLegitRotations(vec);
+        Wrapper.getPlayer().field_71174_a.func_147297_a((Packet)new CPacketPlayer.Rotation(rotations[0], rotations[1], Wrapper.getPlayer().field_70122_E));
+    }
+    
+    private static void processRightClickBlock(final BlockPos pos, final EnumFacing side, final Vec3d hitVec) {
+        getPlayerController().func_187099_a(Wrapper.getPlayer(), BlockInteractionHelper.mc.field_71441_e, pos, side, hitVec, EnumHand.MAIN_HAND);
+    }
+    
+    private static boolean canBeClicked(final BlockPos pos) {
+        return getBlock(pos).func_176209_a(getState(pos), false);
+    }
+    
+    private static Block getBlock(final BlockPos pos) {
+        return getState(pos).func_177230_c();
+    }
+    
+    private static PlayerControllerMP getPlayerController() {
+        return Minecraft.func_71410_x().field_71442_b;
+    }
+    
+    private static IBlockState getState(final BlockPos pos) {
+        return Wrapper.getWorld().func_180495_p(pos);
+    }
+    
+    public static boolean checkForNeighbours(final BlockPos blockPos) {
+        if (!hasNeighbour(blockPos)) {
+            for (final EnumFacing side : EnumFacing.values()) {
+                final BlockPos neighbour = blockPos.func_177972_a(side);
+                if (hasNeighbour(neighbour)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+        return true;
+    }
+    
+    private static boolean hasNeighbour(final BlockPos blockPos) {
+        for (final EnumFacing side : EnumFacing.values()) {
+            final BlockPos neighbour = blockPos.func_177972_a(side);
+            if (!Wrapper.getWorld().func_180495_p(neighbour).func_185904_a().func_76222_j()) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    static {
+        blackList = Arrays.asList(Blocks.field_150477_bB, (Block)Blocks.field_150486_ae, Blocks.field_150447_bR, Blocks.field_150462_ai, Blocks.field_150467_bQ, Blocks.field_150382_bo, (Block)Blocks.field_150438_bZ, Blocks.field_150409_cd, Blocks.field_150367_z);
+        mc = Minecraft.func_71410_x();
+    }
+}
